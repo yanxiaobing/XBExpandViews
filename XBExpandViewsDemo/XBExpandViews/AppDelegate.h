4dd312d215b9b369b1bf300d79ad5914e75a6335
XBingo <dove025@qq.com>
2017-12-20 22:19:29 +0800

    创建工程

---
+++ XBExpandViewsDemo/XBExpandViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBExpandViews
+//
+//  Created by XBingo on 2017/12/20.
+//  Copyright © 2017年 XBingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBExpandViewsDemo/XBExpandViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBExpandViews
+//
+//  Created by XBingo on 2017/12/20.
+//  Copyright © 2017年 XBingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
